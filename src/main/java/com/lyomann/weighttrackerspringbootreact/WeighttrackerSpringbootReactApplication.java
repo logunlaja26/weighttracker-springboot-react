5bb88f28aa714be159edcd9829346201a4e92c03
Ly Ogunlaja <logunlaja26@gmail.com>
2020-07-06 19:00:05 -0500

    first commit

---
+++ src/main/java/com/lyomann/weighttrackerspringbootreact/WeighttrackerSpringbootReactApplication.java
@@ -0,0 +1,13 @@
+package com.lyomann.weighttrackerspringbootreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeighttrackerSpringbootReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeighttrackerSpringbootReactApplication.class, args);
+	}
+
+}
