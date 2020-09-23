d7db98209930825bab0a2aa979682b37fcfa822d
Olayiwola Ogunlaja <logunlaja26@gmail.com>
2020-09-23 09:21:21 -0500

    added new weight

--- src/main/java/com/lyomann/weighttrackerspringbootreact/service/WeightService.java
+++ src/main/java/com/lyomann/weighttrackerspringbootreact/service/WeightService.java
@@ -24,7 +24,8 @@ public class WeightService {
     public List<Weight> getAllWeights(){
         List<Weight> weights = new ArrayList<>();
         weights.add(new Weight(0, weight.getLocalDate()));
-        weights.add(new Weight(0,weight.getLocalDate()));
+        weights.add(new Weight(175,weight.getLocalDate()));
+        weights.add(new Weight(180,weight.getLocalDate()));
       
 
         return weights;
